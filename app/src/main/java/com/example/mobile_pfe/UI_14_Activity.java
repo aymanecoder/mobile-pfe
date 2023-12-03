9c63e2fbd41c749679cf02ab58c40bca55e59b02
John Doe <you@example.com>
2023-12-03 20:36:42 +0000

    screen 1.4 1.5 commitgit

--- app/src/main/java/com/example/mobile_pfe/UI_14_Activity.java
+++ app/src/main/java/com/example/mobile_pfe/UI_14_Activity.java
@@ -6,21 +6,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class UI_14_Activity extends AppCompatActivity {
 
+    String selectedGender = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ui14);
 
+
+        Button maleButton = findViewById(R.id.male_button);
+        Button femaleButton = findViewById(R.id.female_button);
+
+        maleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedGender = "Male";
+                maleButton.setBackground(getResources().getDrawable(R.drawable.clicked_button));
+                femaleButton.setBackground(getResources().getDrawable(R.drawable.button1));
+            }
+        });
+
+        femaleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedGender = "Female";
+                maleButton.setBackground(getResources().getDrawable(R.drawable.button1));
+                femaleButton.setBackground(getResources().getDrawable(R.drawable.clicked_button));
+            }
+        });
+
+
         Button button = findViewById(R.id.next_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(UI_14_Activity.this, UI_15_Activity.class);
-                startActivity(intent);
+                if (selectedGender != null) {
+                    Intent intent = new Intent(UI_14_Activity.this, UI_15_Activity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please choose a gender", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
