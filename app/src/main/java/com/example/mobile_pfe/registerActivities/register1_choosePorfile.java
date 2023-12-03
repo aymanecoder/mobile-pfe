93e936698103834550a2cba5d58114fc78340e99
Laghrissim <mohamed.laghrissi@uit.ac.ma>
2023-12-03 22:01:26 +0100

    Creation of design of layout 1.3

---
+++ app/src/main/java/com/example/mobile_pfe/registerActivities/register1_choosePorfile.java
@@ -0,0 +1,16 @@
+package com.example.mobile_pfe.registerActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobile_pfe.R;
+
+public class register1_choosePorfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register1_choose_porfile);
+    }
+}
