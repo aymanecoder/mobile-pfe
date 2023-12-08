237083c169522b5ff1e9283f50bc5132d80ea63a
yassine <Oussiyassine1@gmail.com>
2023-12-08 19:57:52 +0100

    impelement screen upload program

---
+++ app/src/main/java/com/example/mobile_pfe/programActivity/uploadProgramActivity.java
@@ -0,0 +1,16 @@
+package com.example.mobile_pfe.programActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobile_pfe.R;
+
+public class uploadProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_program);
+    }
+}
