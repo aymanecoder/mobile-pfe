c0f457dc07423b6785bf211a21899b9c8a4d71e1
aymanebiti <aymanbiti2@gmail.com>
2023-12-08 19:10:36 +0100

    fix

---
+++ app/src/main/java/com/example/mobile_pfe/profileActivities/profile.java
@@ -0,0 +1,15 @@
+package com.example.mobile_pfe.profileActivities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobile_pfe.R;
+
+public class profile extends AppCompatActivity {@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_profile);
+}
+
+}
