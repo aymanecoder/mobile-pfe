496881cfbc70533599ec3059910ef13bc2d3f6f5
yassine <Oussiyassine1@gmail.com>
2023-12-08 18:11:19 +0100

    implement screen display program

---
+++ app/src/main/java/com/example/mobile_pfe/programActivity/listProgramActivity.java
@@ -0,0 +1,30 @@
+package com.example.mobile_pfe.programActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.mobile_pfe.R;
+
+public class listProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_program);
+    }
+
+    public void onNutritionClicked(View view) {
+        // Hide Entrainement and show Nutrition section
+//        findViewById(R.id.entrainementSection).setVisibility(View.GONE);
+//        findViewById(R.id.nutritionSection).setVisibility(View.VISIBLE);
+    }
+
+    public void onEntrainementClicked(View view) {
+        // Hide Nutrition and show Entrainement section
+//        findViewById(R.id.nutritionSection).setVisibility(View.GONE);
+//        findViewById(R.id.entrainementSection).setVisibility(View.VISIBLE);
+    }
+
+}
--- app/src/main/java/com/example/mobile_pfe/programActivity/programActivity.java
+++ app/src/main/java/com/example/mobile_pfe/programActivity/programActivity.java
@@ -3,7 +3,6 @@ package com.example.mobile_pfe.programActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.mobile_pfe.R;
 
@@ -14,17 +13,4 @@ public class programActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program);
     }
-
-    public void onNutritionClicked(View view) {
-        // Hide Entrainement and show Nutrition section
-//        findViewById(R.id.entrainementSection).setVisibility(View.GONE);
-//        findViewById(R.id.nutritionSection).setVisibility(View.VISIBLE);
-    }
-
-    public void onEntrainementClicked(View view) {
-        // Hide Nutrition and show Entrainement section
-//        findViewById(R.id.nutritionSection).setVisibility(View.GONE);
-//        findViewById(R.id.entrainementSection).setVisibility(View.VISIBLE);
-    }
-
 }
