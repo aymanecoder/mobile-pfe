a1a3fe0e759549972a8ddd0bba44545c06173dcd
John Doe <you@example.com>
2023-12-13 23:10:01 +0000

    commit

--- app/src/main/java/com/example/mobile_pfe/UI/MainActivity.java
+++ app/src/main/java/com/example/mobile_pfe/UI/MainActivity.java
@@ -1,4 +1,17 @@
 package com.example.mobile_pfe.UI;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mobile_pfe.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
 }
