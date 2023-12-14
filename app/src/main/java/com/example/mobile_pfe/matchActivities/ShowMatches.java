fdfe73f1621ecfe7b48c737b2347b083791677cb
Laghrissim <mohamed.laghrissi@uit.ac.ma>
2023-12-14 21:56:32 +0100

    begin the first match layout

---
+++ app/src/main/java/com/example/mobile_pfe/matchActivities/ShowMatches.java
@@ -0,0 +1,16 @@
+package com.example.mobile_pfe.matchActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mobile_pfe.R;
+
+public class ShowMatches extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_matches);
+    }
+}
