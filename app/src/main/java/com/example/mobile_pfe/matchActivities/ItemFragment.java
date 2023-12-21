b3a08d8669f5c6a209bdeecfede669c5eb4b2326
Laghrissim <mohamed.laghrissi@uit.ac.ma>
2023-12-21 14:39:06 +0100

    transaction to fragmentitem 6.3 , adding Map

--- app/src/main/java/com/example/mobile_pfe/matchActivities/ItemFragment.java
+++ app/src/main/java/com/example/mobile_pfe/matchActivities/ItemFragment.java
@@ -1,32 +1,28 @@
 package com.example.mobile_pfe.matchActivities;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import androidx.fragment.app.Fragment;
 import com.example.mobile_pfe.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ItemFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ItemFragment extends Fragment {
+public class ItemFragment extends Fragment implements OnMapReadyCallback {
 
     private TextView team1NameTextView;
     private TextView team2NameTextView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private GoogleMap googleMap;
+    private MapView mapView;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -34,15 +30,6 @@ public class ItemFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ItemFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ItemFragment newInstance(String param1, String param2) {
         ItemFragment fragment = new ItemFragment();
         Bundle args = new Bundle();
@@ -64,8 +51,19 @@ public class ItemFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_item, container, false);
+
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                mapView = view.findViewById(R.id.mapView);
+                mapView.onCreate(savedInstanceState);
+                mapView.onResume();
+                mapView.getMapAsync(ItemFragment.this);
+            }
+        });
+
+
         team1NameTextView = view.findViewById(R.id.team1Name);
         team2NameTextView = view.findViewById(R.id.team2Name);
 
@@ -83,4 +81,24 @@ public class ItemFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onMapReady(GoogleMap mMap) {
+        googleMap = mMap;
+
+        // You can customize the map settings here
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // Add markers, set camera position, etc.
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mapView != null) {
+            mapView.onDestroy();
+        }
+    }
+
+
 }
