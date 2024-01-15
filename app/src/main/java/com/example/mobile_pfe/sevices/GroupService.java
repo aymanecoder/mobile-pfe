6ed4c5ec458e890d04d287bd9c2fcf24a6514d70
soukaini adil <adilelwerdi81@gmail.com>
2024-01-15 21:56:28 +0100

    feat : joining my user to a group

--- app/src/main/java/com/example/mobile_pfe/sevices/GroupService.java
+++ app/src/main/java/com/example/mobile_pfe/sevices/GroupService.java
@@ -1,8 +1,10 @@
 package com.example.mobile_pfe.sevices;
 
 
+import com.example.mobile_pfe.GroupActivity.User;
 import com.example.mobile_pfe.model.Group;
 import com.example.mobile_pfe.model.GroupDTO;
+import com.example.mobile_pfe.model.Sportif;
 
 import java.util.List;
 
@@ -19,7 +21,10 @@ public interface GroupService {
     @GET("/api/v1/groups")
     Call<List<GroupDTO>> getAllGroups();
 
-    @PUT("/groups/{id}")
+    @PUT("/api/v1/groups/{id}")
     Call<GroupDTO> updateGroup(@Path("id") Long id, @Body GroupDTO updatedGroupDto);
+
+    @POST("/api/v1/groups/{userId}/join/{groupId}")
+    Call<Sportif> joinGroup(@Path("userId") int userId, @Path("groupId") Long groupId);
 }
 
