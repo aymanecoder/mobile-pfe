9e28636a39e7178a483a325d1701c4787be236e5
yassine <Oussiyassine1@gmail.com>
2024-01-16 01:56:03 +0100

    get competitions by sport and fix conflicts

--- app/src/main/java/com/example/mobile_pfe/Network/RetrofitInstance.java
+++ app/src/main/java/com/example/mobile_pfe/Network/RetrofitInstance.java
@@ -16,7 +16,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
 
     private static Retrofit retrofit;
-    public static final String BASE_URL = "http://192.168.1.177:8080/";
+    public static final String BASE_URL = "http://192.168.1.103:8080/";
     public static final String BASE_URL_IP = "192.168.1.177";
 
     private static final String AUTH_TOKEN = AppGlobals.getAccessToken();
